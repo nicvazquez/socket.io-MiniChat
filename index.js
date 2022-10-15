fc062d5fb6cd5bf671e06be07125ea372093d116
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-15 16:31:04 -0300

    Send and receive events

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const io = require("socket.io")(server);
 // Deploy public directory
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", () => {
-	console.log("Client connected!");
+io.on("connection", (socket) => {
+	socket.emit("welcome-message", {
+		msg: "Welcome to the server",
+		date: new Date(),
+	});
 });
+
 server.listen(8080, () => {
 	console.log("Server running ...");
 });
